3f62c71a84def9968a797d7f9b2e17ed1a0fd42f
Burkhard Stubert <burkhard.stubert@embeddeduse.com>
2022-07-29 18:38:03 +0200

    Fixed the crash from accessing STM32-specific memory through SPI3. Fix: Introduce global variable of type SPI_TypeDef and use its address for SPI3.

--- examples/mock-spi/tests/tst_full_duplex_spi.cpp
+++ examples/mock-spi/tests/tst_full_duplex_spi.cpp
@@ -8,6 +8,8 @@ extern "C"
 #include "stm32f2xx_hal_conf.h"
 }
 
+#define SPIx SPI3
+
 class TestSpiMaster : public testing::Test
 {
 protected:
@@ -22,8 +24,68 @@ protected:
     }
 };
 
+/*
+ * The HAL_SPI_Init call crashes on __HAL_SPI_DISABLE(hspi), which is defined as
+ *
+ *     CLEAR_BIT((__HANDLE__)->Instance->CR1, SPI_CR1_SPE)
+ *
+ * The crash is triggered by
+ *
+ *     (__HANDLE__)->Instance->CR1
+ *
+ * because (__HANDLE__)->Instance points to a STM32-specific memory address. This address
+ * doesn't make any sense on a x86_64 computer.
+ *
+ * The memory address resolves to SPI3.
+ *
+ *     (__HANDLE__)->Instance == handle.instance == SPIx == SPI3
+ *
+ * SPI3 is defined in stm32f205xx.h as
+ *
+ *     SPI3                  ((SPI_TypeDef *) SPI3_BASE)
+ *
+ * where SPI3_BASE resolves as
+ *
+ *     SPI3_BASE             (APB1PERIPH_BASE + 0x3C00UL)
+ *     APB1PERIPH_BASE       PERIPH_BASE
+ *     PERIPH_BASE           0x40000000UL
+ *
+ * The structure is defined in the same file. It has a field
+ *
+ *     // SPI control register 1 (not used in I2S mode),      Address offset: 0x00
+ *     __IO uint32_t CR1;
+ *
+ * The structure has 9 registers each of type uint32_t (4 bytes). The first field has the address
+ * SPI3_BASE = 0x40003C00UL, the second at SPI3_BASE + 4, the third at SPI3_BASE + 4, etc. The
+ * total size of the structure is 9 * 4 = 36 bytes.
+ *
+ * We double the header stm32f205xx.h, because we must make SPI3 point to an address
+ * of the x86_64 computer. As stm32f2xx.h includes stm32f2xx.h and both are in the same directory,
+ * we must also double stm32f2xx.h.
+ *
+ * In stm32f205xx.h, we instantiate a global variable and assign its address to SPI3.
+ *
+ *     SPI_TypeDef spi3_base = {0};
+ *     #define SPI3 (&spi3_base);
+ *
+ * We comment out the original definitions of SPI3_BASE and SPI3. The init test runs without
+ * crashing.
+ *
+ */
 TEST_F(TestSpiMaster, init)
 {
-    SPI_HandleTypeDef hspi;
-    EXPECT_EQ(HAL_SPI_Init(&hspi), HAL_OK);
+    SPI_HandleTypeDef handle;
+    handle.Instance               = SPIx;
+    handle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+    handle.Init.Direction         = SPI_DIRECTION_2LINES;
+    handle.Init.CLKPhase          = SPI_PHASE_2EDGE;
+    handle.Init.CLKPolarity       = SPI_POLARITY_LOW;
+    handle.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
+    handle.Init.CRCPolynomial     = 7;
+    handle.Init.DataSize          = SPI_DATASIZE_8BIT;
+    handle.Init.FirstBit          = SPI_FIRSTBIT_MSB;
+    handle.Init.NSS               = SPI_NSS_SOFT;
+    handle.Init.TIMode            = SPI_TIMODE_DISABLE;
+    handle.Init.Mode              = SPI_MODE_MASTER;
+    EXPECT_EQ(HAL_SPI_Init(&handle), HAL_OK);
 }
